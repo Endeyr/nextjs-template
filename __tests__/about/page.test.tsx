18d0b53bce1f74c26c6579201860a9416d7baf82
Endeyr <aaron.thompson42594@gmail.com>
2025-02-10 12:01:30 -0500

    feat: add test for about page

--- __tests__/about/page.test.tsx
+++ __tests__/about/page.test.tsx
@@ -1,10 +1,10 @@
-import Page from '@/app/page';
+import AboutPage from '@/app/about/page';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
 describe('Page', () => {
   it('renders a heading', () => {
-    render(<Page />);
+    render(<AboutPage />);
 
     const heading = screen.getByRole('heading', { level: 1 });
 
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function Page() {
+export default function AboutPage() {
   return (
     <div>
       <h1>About</h1>
